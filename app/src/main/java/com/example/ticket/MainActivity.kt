617645930e7da83836bfd8d4ccb490dbbe25c116
Manojmar <manojmadakannu44@gmail.com>
2022-07-20 21:05:33 +0530

    card

--- app/src/main/java/com/example/ticket/MainActivity.kt
+++ app/src/main/java/com/example/ticket/MainActivity.kt
@@ -17,5 +17,6 @@ class MainActivity : AppCompatActivity() {
         modal=ArrayList()
         modal.add(Modal("MAA-DXB-FRA","25jul,2022(6:30 PM)to 05Aug,2022(11:00 AM)","Passenger","2Adults,2Children","Total Services","4Services","you Will earn 65 coins","₹ 5,520"))
         mainBinding.recuy.adapter=Adapter(modal)
+
     }
 }
