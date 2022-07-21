e97e129783b75af1ac48d5ec4974524c92bf5175
Manojmar <manojmadakannu44@gmail.com>
2022-07-21 17:33:03 +0530

    login

--- app/src/main/java/com/example/ticket/Adapter.kt
+++ app/src/main/java/com/example/ticket/Adapter.kt
@@ -1,5 +1,6 @@
 package com.example.ticket
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -18,9 +19,13 @@ class Adapter(var data:List<Modal>): RecyclerView.Adapter<Adapter.ViewHolder>() 
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(data[position])
-    }
+
+        holder.itemView.setOnClickListener {
+                holder.itemView.context.startActivity(Intent(holder.itemView.context,Login::class.java))
+            }
+        }
 
     override fun getItemCount(): Int {
         return data.size
     }
-}
+    }
